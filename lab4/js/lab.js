bb565091c2ef667ee935ac811063a720b6480ac2
KarlzJr <67616441+KarlzJr@users.noreply.github.com>
2022-04-15 21:04:08 -0700

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,11 @@
+// Summary: Lab 4 - Pseudocoding and Problem-solving
+// Description: A computer game
+// @author Alejandra Sicairos and Alice Aiba
+// @since  4.15.22
+
+// Get cards
+    // Check one card value and keep the other card face down
+        // Guess total number added up
+        // Ask for cards if necessary
+            // Claim for final card pairs
+        // Flip the other card and compare
