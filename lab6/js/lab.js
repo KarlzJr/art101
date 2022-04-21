d29abc8251a08bc406231a4a2e8323991e75afbf
KarlzJr <67616441+KarlzJr@users.noreply.github.com>
2022-04-21 08:11:23 -0700

    Update lab.js

---
+++ lab6/js/lab.js
@@ -0,0 +1,6 @@
+// Summary: Lab 6: Arrays and Objects
+// Description: Experiment with basic JavaScript data types and variables.
+// @author Alejandra Sicairos and Alice Aiba
+// @created  4.25.22
+
+//REMEMBER TO PUT A SEMICOLON AT THE END OF EVERY LINE!
