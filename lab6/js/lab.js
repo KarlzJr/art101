4b710dbf5ea9dab772729b6dd4c2c9d74d13f798
KarlzJr <67616441+KarlzJr@users.noreply.github.com>
2022-04-25 14:44:19 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -4,3 +4,21 @@
 // @created  4.25.22
 
 //REMEMBER TO PUT A SEMICOLON AT THE END OF EVERY LINE!
+
+//Declare array myTransport
+var myTransport = ['bike', 'bus', 'walking', 'car'];
+
+//Declare object myMainRide
+var myMainRide = {
+    make: "Mercedes",
+    model: "C230 Kompressor",
+    color: "Black",
+    year: 2005,
+    age: function() {
+        return 2022 - age;
+    }
+}
+//Output
+document.writeln('My kinds of transports: ' + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
