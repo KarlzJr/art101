a0540eaa077f13b2e84cb994ba41d41b019ccbae
KarlzJr <67616441+KarlzJr@users.noreply.github.com>
2022-05-10 15:28:41 -0700

    update

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -3,4 +3,12 @@
 // @author Alejandra Sicairos and Alice Aiba
 // @created  5.10.22
 
-$(".content p").append("<button>Press") 
+$("#content p").append("<button>Press");
+
+$("button").click(function(){
+  $("#content p").toggleClass("selected")
+});
+
+$("button").click(function(){
+  $("button").toggleClass("buttselected")
+});
